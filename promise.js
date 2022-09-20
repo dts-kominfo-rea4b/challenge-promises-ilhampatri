a4a095b2c373fd2bf5dc93fe92942ddc88ef3dd2
patriilham <patriilham002@gmail.com>
2022-09-20 19:02:56 +0700

    promise

--- promise.js
+++ promise.js
@@ -1,8 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
-
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+var totalEmosi =0;
+await promiseTheaterIXX().then((result) => {
+  for (let index = 0; index < result.length; index++) {
+    if (result[index].hasil == emosi) {
+      totalEmosi += 1;
+    }
+  }
+})
 
-module.exports = {
-  promiseOutput,
+await promiseTheaterVGC().then((result) => {
+  for (let index = 0; index < result.length; index++) {
+    if (result[index].hasil == emosi) {
+      totalEmosi += 1;
+    }
+  }
+})
+return totalEmosi;
+
+}
+  module.exports = {
+  promiseOutput
 };
+
